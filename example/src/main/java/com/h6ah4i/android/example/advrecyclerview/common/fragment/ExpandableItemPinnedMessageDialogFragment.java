abd49fc323e2f0dbbfbef7c4e143ece2980014ec
Haruki Hasegawa <h6a.h4i.0@gmail.com>
2015-02-10 17:33:21 +0900

    Fix AS warnings of "@NotNull/@Nullable problems"

--- example/src/main/java/com/h6ah4i/android/example/advrecyclerview/common/fragment/ExpandableItemPinnedMessageDialogFragment.java
+++ example/src/main/java/com/h6ah4i/android/example/advrecyclerview/common/fragment/ExpandableItemPinnedMessageDialogFragment.java
@@ -20,6 +20,8 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.RecyclerView;
 
@@ -48,8 +50,9 @@ public class ExpandableItemPinnedMessageDialogFragment extends DialogFragment {
         super();
     }
 
+    @NonNull
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         final int groupPosition = getArguments().getInt(KEY_GROUP_ITEM_POSITION, Integer.MIN_VALUE);
--- example/src/main/java/com/h6ah4i/android/example/advrecyclerview/common/fragment/ItemPinnedMessageDialogFragment.java
+++ example/src/main/java/com/h6ah4i/android/example/advrecyclerview/common/fragment/ItemPinnedMessageDialogFragment.java
@@ -20,6 +20,8 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 
 import com.h6ah4i.android.example.advrecyclerview.R;
@@ -45,8 +47,9 @@ public class ItemPinnedMessageDialogFragment extends DialogFragment {
         super();
     }
 
+    @NonNull
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         final int itemPosition = getArguments().getInt(KEY_ITEM_POSITION, Integer.MIN_VALUE);
