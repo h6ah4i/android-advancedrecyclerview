fe0b220abf16ed4703652adf12266d079069c55a
Haruki Hasegawa <h6a.h4i.0@gmail.com>
2015-09-30 22:08:05 +0900

    Fix header comment block

--- library/src/main/java/com/h6ah4i/android/widget/advrecyclerview/swipeable/SwipeableItemInternalUtils.java
+++ library/src/main/java/com/h6ah4i/android/widget/advrecyclerview/swipeable/SwipeableItemInternalUtils.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright (C) 2015 Haruki Hasegawa
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.h6ah4i.android.widget.advrecyclerview.swipeable;
 
 import android.support.v7.widget.RecyclerView;
@@ -5,9 +21,6 @@ import android.support.v7.widget.RecyclerView;
 import com.h6ah4i.android.widget.advrecyclerview.swipeable.action.LegacySwipeResultAction;
 import com.h6ah4i.android.widget.advrecyclerview.swipeable.action.SwipeResultAction;
 
-/**
- * Created by hasegawa on 9/20/15.
- */
 public class SwipeableItemInternalUtils {
     private SwipeableItemInternalUtils() {
     }
