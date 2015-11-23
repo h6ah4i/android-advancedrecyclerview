cfe8f2eed9be17a32f87b0c5dfc66c4e99af4a76
Haruki Hasegawa <h6a.h4i.0@gmail.com>
2015-11-23 21:33:54 +0900

    Remove vector-compat library
    Because vector-compat is incompatible with Android Plugin for Gradle v1.5.0,
    and not working on Marshmallow (#149).
    
    I decided to use native VectorDrawable, so animation is only work on Lollipop or later.

--- example/src/main/java/com/h6ah4i/android/example/advrecyclerview/common/widget/ExpandableItemIndicatorImplAnim.java
+++ example/src/main/java/com/h6ah4i/android/example/advrecyclerview/common/widget/ExpandableItemIndicatorImplAnim.java
@@ -18,30 +18,40 @@ package com.h6ah4i.android.example.advrecyclerview.common.widget;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.drawable.Animatable;
 import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.h6ah4i.android.example.advrecyclerview.R;
-import com.wnafee.vector.MorphButton;
 
-@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 class ExpandableItemIndicatorImplAnim extends ExpandableItemIndicator.Impl {
-    private MorphButton mMorphButton;
+    private ImageView mImageView;
+    private int mColor;
 
     @Override
     public void onInit(Context context, AttributeSet attrs, int defStyleAttr, ExpandableItemIndicator thiz) {
-        View v = LayoutInflater.from(context).inflate(R.layout.widget_expandable_item_indicator_anim, thiz, true);
-        mMorphButton = (MorphButton) v.findViewById(R.id.morph_button);
+        View v = LayoutInflater.from(context).inflate(R.layout.widget_expandable_item_indicator, thiz, true);
+        mImageView = (ImageView) v.findViewById(R.id.image_view);
+        mColor = ContextCompat.getColor(context, R.color.expandable_item_indicator_color);
     }
 
     @Override
     public void setExpandedState(boolean isExpanded, boolean animate) {
-        MorphButton.MorphState indicatorState = (isExpanded) ? MorphButton.MorphState.START : MorphButton.MorphState.END;
-
-        if (mMorphButton.getState() != indicatorState) {
-            mMorphButton.setState(indicatorState, animate);
+        if (animate) {
+            int resId = isExpanded ? R.drawable.ic_expand_more_to_expand_less : R.drawable.ic_expand_less_to_expand_more;
+            mImageView.setImageResource(resId);
+            DrawableCompat.setTint(mImageView.getDrawable(), mColor);
+            ((Animatable) mImageView.getDrawable()).start();
+        } else {
+            int resId = isExpanded ? R.drawable.ic_expand_less_vector : R.drawable.ic_expand_more_vector;
+            mImageView.setImageResource(resId);
+            DrawableCompat.setTint(mImageView.getDrawable(), mColor);
         }
     }
 }
